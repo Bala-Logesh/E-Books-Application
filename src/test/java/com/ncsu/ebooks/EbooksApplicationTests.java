578521e06240237a19ac3c55b460c2e585826f1b
bsudala <bsudala@ncsu.edu>
2024-10-22 12:28:11 -0400

    Initial Commit

---
+++ src/test/java/com/ncsu/ebooks/EbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncsu.ebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
