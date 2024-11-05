9ab75b805b1c425e11473ee593641911341dce6b
vignes-12 <obblivignes.kv@gmail.com>
2024-11-04 23:33:36 -0500

    Added initial Database_Setup.sql and README.md (need to modify Sample_Data.sql according to schema)

---
+++ Database_Setup.sql
@@ -0,0 +1,208 @@
+CREATE DATABASE ebooks;
+
+USE ebooks;
+
+CREATE TABLE IF NOT EXISTS ETextBook (
+    eTextBookID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(255) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS Chapter (
+    chapterID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    chapterNumber VARCHAR(6) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    eTextBookID INT NOT NULL,
+    hidden BOOLEAN DEFAULT 0,
+    PRIMARY KEY (chapterID),
+    UNIQUE (title, chapterNumber, eTextBookID),
+    FOREIGN KEY (eTextBookID) REFERENCES ETextBook(eTextBookID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CHECK (chapterNumber REGEXP '^chap[0-9]{2,}$'));
+
+CREATE TABLE IF NOT EXISTS Section (
+    sectionID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    sectionNumber VARCHAR(6) NOT NULL,
+    chapterID INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    hidden BOOLEAN DEFAULT 0,
+    PRIMARY KEY (sectionID),
+    UNIQUE (title, sectionNumber, chapterID),
+    FOREIGN KEY (chapterID) REFERENCES Chapter(chapterID)
+    ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS ContentBlock (
+    ContentBlockID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    sectionID INT NOT NULL,
+    image BLOB,
+    textBlock LONGTEXT,
+    hidden BOOLEAN DEFAULT 0,
+    PRIMARY KEY (contentBlockID),
+    UNIQUE (contentBlockID, sectionID),
+    FOREIGN KEY (sectionID) REFERENCES Section(sectionID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CHECK (image IS NOT NULL OR textBlock IS NOT NULL));
+
+CREATE TABLE IF NOT EXISTS Activity (
+    activityID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    sectionID INT NOT NULL,
+    contentBlockID INT NOT NULL,
+    question LONGTEXT NOT NULL,
+    points INT NOT NULL DEFAULT 0,
+    hidden BOOLEAN DEFAULT 0,
+    PRIMARY KEY (activityID),
+    UNIQUE (activityID, sectionID),
+    FOREIGN KEY (contentBlockID) REFERENCES ContentBlock(contentBlockID)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (sectionID) REFERENCES Section(sectionID)
+    ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS AnswerSet (
+    answerSetID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    activityID INT NOT NULL,
+    answerOption LONGTEXT NOT NULL,
+    correct BOOLEAN NOT NULL DEFAULT 0,
+    explanation LONGTEXT NOT NULL,
+    PRIMARY KEY (activityID, answerSetID),
+    FOREIGN KEY (activityID) REFERENCES Activity(activityID)
+    ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS User (
+    userID VARCHAR(8) NOT NULL UNIQUE,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    accountCreation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role ENUM('ADMIN', 'FACULTY', 'TEACHING_ASSISTANT', 'STUDENT') NOT NULL,
+    PRIMARY KEY (userID));
+
+CREATE TABLE IF NOT EXISTS User (
+    userID VARCHAR(8) NOT NULL UNIQUE,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    accountCreation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role ENUM('ADMIN', 'FACULTY', 'TEACHING_ASSISTANT', 'STUDENT') NOT NULL,
+    PRIMARY KEY (userID));
+
+CREATE TABLE IF NOT EXISTS Admin (
+    adminID INT NOT NULL AUTO_INCREMENT,
+    userID VARCHAR(8) NOT NULL,
+    PRIMARY KEY (adminID),
+    FOREIGN KEY (UserID) REFERENCES User(userID)
+    ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE IF NOT EXISTS Faculty (
+    facultyId INT NOT NULL AUTO_INCREMENT,
+    userID VARCHAR(8) NOT NULL,
+    PRIMARY KEY (facultyId),
+    FOREIGN KEY (UserID) REFERENCES User(userID)
+    ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE IF NOT EXISTS Student (
+    studentID INT NOT NULL AUTO_INCREMENT,
+    userID VARCHAR(8) NOT NULL,
+    PRIMARY KEY (studentID),
+    FOREIGN KEY (UserID) REFERENCES User(userID)
+    ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE IF NOT EXISTS Course (
+    courseID VARCHAR(30) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    facultyID INT NOT NULL,
+    startDate DATETIME NOT NULL,
+    endDate DATETIME NOT NULL,
+    eTextBookID INT NOT NULL,
+    PRIMARY KEY (courseID),
+    FOREIGN KEY (facultyID) REFERENCES Faculty(facultyID)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (eTextBookID) REFERENCES ETextBook(eTextBookID)
+    ON UPDATE CASCADE ON DELETE RESTRICT);
+
+CREATE TABLE IF NOT EXISTS EvaluationCourse (
+    evaluationID INT NOT NULL AUTO_INCREMENT,
+    courseID VARCHAR(30) NOT NULL,
+    PRIMARY KEY (evaluationID),
+    FOREIGN KEY (courseID) REFERENCES Course(courseID)
+    ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS ActiveCourse (
+    activeCourseID INT AUTO_INCREMENT NOT NULL,
+    courseID VARCHAR(30) NOT NULL,
+    capacity INT NOT NULL,
+    token VARCHAR(7) NOT NULL,
+    openToEnroll BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (activeCourseID),
+    FOREIGN KEY (courseID) REFERENCES Course(courseID)
+    ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS TeachingAssistant (
+    teachingAsstID INT NOT NULL AUTO_INCREMENT,
+    userID VARCHAR(8) NOT NULL,
+    activeCourseID INT NOT NULL,
+    resetPassword BOOLEAN DEFAULT 1,
+    PRIMARY KEY (teachingAsstID),
+    FOREIGN KEY (userID) REFERENCES User(userID)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (activeCourseID) REFERENCES ActiveCourse(activeCourseID)
+    ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE IF NOT EXISTS Enrolled (
+    enrolledID INT NOT NULL AUTO_INCREMENT,
+    studentID INT NOT NULL,
+    activeCourseID INT NOT NULL,
+    courseScore INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (enrolledID),
+    FOREIGN KEY (studentID) REFERENCES Student(studentID)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (activeCourseID) REFERENCES ActiveCourse(activeCourseID)
+    ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE IF NOT EXISTS Wait (
+    waitListID INT NOT NULL AUTO_INCREMENT,
+    studentID INT NOT NULL,
+    activeCourseID INT NOT NULL,
+    PRIMARY KEY (waitListId),
+    FOREIGN KEY (studentID) REFERENCES Student(studentID)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (activeCourseID) REFERENCES ActiveCourse(activeCourseID)
+    ON DELETE CASCADE ON UPDATE CASCADE);
+
+CREATE TABLE IF NOT EXISTS Notification (
+    notificationID INT NOT NULL AUTO_INCREMENT,
+    userID VARCHAR(8) NOT NULL,
+    message LONGTEXT NOT NULL,
+    messageRead BOOLEAN DEFAULT 0,
+    PRIMARY KEY (notificationID),
+    FOREIGN KEY (userID) REFERENCES User(userID)
+    ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE IF NOT EXISTS StudentScore (
+    scoreID INT NOT NULL AUTO_INCREMENT,
+    studentID INT NOT NULL,
+    activeCourseID INT NOT NULL,
+    activityID INT NOT NULL,
+    point INT DEFAULT 0,
+    PRIMARY KEY (scoreID),
+    FOREIGN KEY (studentID) REFERENCES Student(studentID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (activityID) REFERENCES Activity(activityID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (activeCourseID) REFERENCES ActiveCourse(activeCourseID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    timeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (studentID, activeCourseID, activityID));
+
+CREATE TABLE IF NOT EXISTS StudentScoreSummary (
+    ssummaryID INT NOT NULL AUTO_INCREMENT,
+    studentID INT NOT NULL,
+    activeCourseID INT NOT NULL,
+    totalPoints INT NOT NULL DEFAULT 0,
+    totalActivities INT DEFAULT 0,
+    PRIMARY KEY (ssummaryID),
+    FOREIGN KEY (studentID) REFERENCES Student(studentID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (activeCourseID) REFERENCES ActiveCourse(activeCourseID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    timeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (studentID, activeCourseID));
---
+++ README.md
@@ -0,0 +1,24 @@
+# CSC 540: Database Management Systems
+## Project 1: Final Deliverable
+### Group Members
+
+Bala Logesh Sudalaimuthu Pandian
+
+Obblivignes KanchandeviVenkataraman
+
+Chinmay Walinjkar
+
+### Description
+
+This GitHub repository contains the SQL code 
+used to define the database schema, the Java 
+code used to connect to the database (backend),
+and the JavaScript web application that we use
+as our front-end.
+
+### Requirements
+
+- This can run on an system that has a JDK version 17+.
+- After running the main EbooksApplication.java, log in 
+to our browser using the url `url` in order to access the 
+front-end application.
